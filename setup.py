9a38675e36f2166b9e7afd65a3b018d98e4b6dc1
Rehan8903 <rehansarfraz8903@gmail.com>
2026-07-19 14:12:34 +0500

    project folders and venev setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rehan Sarfraz",
+    author_email="rehansarfraz012345@gmail.com",
+    packages=find_packages()
+)
